0074055ae8b5a453a1265ff3e3c449f5ea321478
Shubham Garg <unknowngarg2001@gmail.com>
2025-02-17 00:40:33 +0530

    Add files via upload
    commit

---
+++ EvManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.evmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvManagementSystemApplication.class, args);
+	}
+
+}
